5580c59ff6d1004f8cd8de4517efac889158c7a1
Randy Frias <69165606+randy140801@users.noreply.github.com>
2022-08-16 09:02:54 -0400

    Update EmpleadosContext.cs

--- EmpleadosApi/Models/EmpleadosContext.cs
+++ EmpleadosApi/Models/EmpleadosContext.cs
@@ -23,7 +23,7 @@ namespace EmpleadosApi.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=LAPTOP-F91TQ22G\\RANDYSERVER;Database=Cars;User=LAPTOP-F91TQ22G\\Randy;Trusted_Connection=true;MultipleActiveResultSets=true;");
+                optionsBuilder.UseSqlServer("Server=LAPTOP-F91TQ22G\\RANDYSERVER;Database=Empleados;User=LAPTOP-F91TQ22G\\Randy;Trusted_Connection=true;MultipleActiveResultSets=true;");
             }
         }
 
